aaad61efc6d31ba4a793692f484f13ed2c673df8
punkill <giratina54@hotmail.fr>
2021-02-09 14:26:42 +0100

    Home with error not found

--- src/controller/ControllerStream.php
+++ src/controller/ControllerStream.php
@@ -15,12 +15,24 @@ class ControllerStream
     public function home(Request $req, Response $res): Response
     {
         $streams = Stream::all();
-        $res->getBody()->write(json_encode($streams));
-        return $res;
+        if(!is_null($streams))
+        {
+            $res = $res->withStatus(200)
+                        ->withHeader('Content-Type','application/json');
+            $res->getBody()->write(json_encode($streams));
+            return $res;
+        }
+        else
+        {
+            $res = $res->withStatus(404)                     
+                        ->withHeader('Content-Type','application/json');
+            $res->getBody()->write('<h1>Streams not found</h1>');
+            return $res;
+        }
     }
 
     public function getStream(Request $req, Response $res,array $args): Response
     {
-        
+        //Developpement ici de WebRTC
     }
 }
