9c1b7df23fb8a15a6670b980256ed8004109459b
Alex Bastien <onsenblc@outlook.fr>
2021-02-17 15:37:15 +0100

    stream

--- src/model/Stream.php
+++ src/model/Stream.php
@@ -3,6 +3,7 @@
 namespace projet\model;
 
 class Stream extends \Illuminate\Database\Eloquent\Model {
+    public $incrementing = false;
     protected $table = 'stream';
     protected $primaryKey = 'id';
     public    $timestamps = true;
